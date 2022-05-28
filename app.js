da360d3dda0bc2f91576ba56983e180e982cd076
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-05-28 20:48:47 +0700

    Improved Stripe implementation

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use('/api', limiter);
 
 app.post(
   '/webhook-checkout',
-  express.raw({ type: 'application / json' }),
+  express.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
 
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.line_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -62,7 +62,7 @@ exports.webhookCheckout = (req, res, next) => {
 
   let event;
   try {
-    event = stripe.webhook.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
