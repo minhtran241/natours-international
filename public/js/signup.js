7ede09985820c0f9338698efc4423a3ad8ed9de9
Minh Tran <98200886+minhtran241@users.noreply.github.com>
2022-05-28 15:45:32 +0700

    Building bundle.js file 🔒

--- public/js/signup.js
+++ public/js/signup.js
@@ -18,7 +18,7 @@ export const signup = async (name, email, password, passwordConfirm) => {
       showAlert('success', 'Account created successfully');
       window.setTimeout(() => {
         location.assign('/');
-      }, 1500);
+      }, 500);
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
